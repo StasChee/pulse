1900fedab859f342fb5b882de451ff8d6f80e007
Stas <texasrv@mail.ru>
2020-07-22 19:06:43 +0300

    carousel slick_slider

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.carousel__inner').slick({
+        speed: 200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/right.png"></button>',
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                dots: true,
+                arrows: false
+            }
+        }]
+    });
+});
